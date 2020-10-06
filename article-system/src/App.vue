628ebcafcfee8af39a033204d81ba45a190e419d
zxwin <zxwin_0125@163.com>
2020-10-06 19:48:23 +0800

    :fire:整体页面

--- article-system/src/App.vue
+++ article-system/src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ResourceHome />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ResourceHome from '@/views/ResourceHome.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ResourceHome,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 </style>
