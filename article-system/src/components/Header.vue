2fd6504f814b6e9aa38db673e44beda36ef5857c
zxwin <zxwin_0125@163.com>
2020-10-06 19:50:51 +0800

    :fir:Header部分

---
+++ article-system/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- header starts -->
+  <div class="py-5 text-center">
+    <img height="150" src="@/assets/logo1.jpg" alt />
+    <h2>小鑫文章栏</h2>
+    <p class="lead">以人为镜可明得失,以代码为镜可通逻辑!</p>
+    <router-link
+      class="btn btn-outline-primary mr-2"
+      :to="{ name: 'resourceHomePage' }"
+      >首页</router-link
+    >
+    <router-link
+      class="btn btn-outline-primary"
+      :to="{ name: 'resourceNewPage' }"
+      >添加</router-link
+    >
+  </div>
+  <!-- header ends -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
