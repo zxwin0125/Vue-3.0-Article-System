3b60caf02a23b7cb386251ccda08325238b41bf9
zxwin <zxwin_0125@163.com>
2020-10-06 23:59:32 +0800

    :fire:实现列表数据统计

--- article-system/src/views/ResourceHome.vue
+++ article-system/src/views/ResourceHome.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="container">
-    {{resources}}
     <Header />
     <div class="row">
       <div class="col-md-4 order-md-2 mb-4">
         <h4 class="d-flex justify-content-between align-items-center mb-3">
           <span class="text-muted">数据</span>
-          <span class="badge badge-secondary badge-pill">6</span>
+          <span class="badge badge-secondary badge-pill">{{getResourcesLength()}}</span>
         </h4>
         <ResourceSearch />
 
@@ -26,7 +25,7 @@
 </template>
 
 <script>
-import { toRefs, reactive } from 'vue';
+import { toRefs, reactive, computed } from 'vue';
 import Header from "@/components/Header.vue";
 import ResourceSearch from "@/components/ResourceSearch.vue";
 import ResourceList from "@/components/ResourceList.vue";
@@ -43,55 +42,70 @@ export default {
     // ResourceDetail,
   },
   setup(){
+      // 模拟列表数据
       const data = reactive({
           resources: [
             {
               _id: "1",
-              title: "米修在线1",
-              description: "何以解忧 唯有米修",
+              title: "2021 前端面试 | “HTML + CSS + JS”专题",
+              description: "BAT面试1000题——数据结构（841~850题）",
               type: "video",
               link: ""
             },
             {
               _id: "2",
-              title: "米修在线2",
-              description: "何以解忧 唯有米修",
+              title: "一篇搞定前端高频手撕算法题（36道）",
+              description: "《JavaScript 20 年》中文在线版发布",
               type: "book",
               link: ""
             },
             {
               _id: "3",
-              title: "米修在线3",
-              description: "何以解忧 唯有米修",
+              title: "32个手撕JS，彻底摆脱初级前端（面试高频）",
+              description: "56 道高频 JavaScript 与 ES6+ 的面试题及答案",
               type: "video",
               link: ""
             },
             {
               _id: "4",
-              title: "米修在线4",
-              description: "何以解忧 唯有米修",
+              title: "字节跳动2020届秋招提前批前端面经",
+              description: "初入WEB前端的新手，掌握这些核心知识点，年薪冲破20W",
               type: "book",
               link: ""
             },
             {
               _id: "5",
-              title: "米修在线5",
-              description: "何以解忧 唯有米修",
+              title: "前方预警！史上最全前端面试题来袭！（附答案）",
+              description: "做一个数据可视化项目的难点在什么地方？",
               type: "video",
               link: ""
             },
             {
               _id: "6",
-              title: "米修在线6",
-              description: "何以解忧 唯有米修",
+              title: "JavaScript数据类型详解",
+              description: "nodejs的websocket的服务器端是如何实现的？",
               type: "book",
               link: ""
             },
           ]
       })
+
+      // 列表数量统计（方法实现）
+      // const getResourcesLength = () => {
+      //   return data.resources.length
+      // }
+
+      // 列表数量统计（计算属性实现）
+      const getResourcesLength = computed(() => {
+        return data.resources.length
+      })
+
+      // 导出数据
       return { 
         // 解包
         ...toRefs(data),
+
+        getResourcesLength,
       }
   }
 };
