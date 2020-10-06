2cff8f545b1c14ca2fc7ad648bbfa7cad7d70f06
zxwin <zxwin_0125@163.com>
2020-10-06 20:09:48 +0800

    :fire:数据详情

---
+++ article-system/src/components/ResourceDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- {/* 数据详情 Starts */} -->
+  <div class="card" v-if="!resource?._id">
+    <div class="card-body">No Resource is selected :(</div>
+  </div>
+  <div v-else class="card">
+    <div class="card-header">{{ resource.title }}</div>
+    <div class="card-body">
+      <blockquote class="blockquote mb-0">
+        <p>{{ resource.description }}</p>
+        <footer class="text-muted mb-2">{{ resource.type }}</footer>
+      </blockquote>
+      <a
+        target="_blank"
+        :href="resource.link"
+        class="btn btn-outline-primary mr-2"
+        >访问</a
+      >
+      <slot name="buttonLink"></slot>
+    </div>
+  </div>
+
+  <!-- {/* 数据详情 Ends */} -->
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      // type: Object,
+      validator: (prop) => typeof prop === "object" || prop === null,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
--- article-system/src/views/ResourceHome.vue
+++ article-system/src/views/ResourceHome.vue
@@ -11,47 +11,13 @@
           <ResourceList />
         </div>
         <!-- {/* 更新数据 Starts */} -->
-        <!-- {/* <div class="col-md-8 order-md-1">
-            <h4 class="mb-3">更新数据</h4>
-            <form>
-              <div class="mb-3">
-                <label htmlFor="firstName">用户名</label>
-                <input type="text" class="form-control" id="firstName" placeholder="用户名...." />
-              </div>
-              <div class="mb-3">
-                <label htmlFor="email">邮箱<span class="text-muted">(Optional)</span></label>
-                <input type="email" class="form-control" id="email" placeholder="邮箱..." />
-              </div>
-              <div class="mb-3">
-                <label for="description">描述</label>
-                <textarea class="form-control" id="description" placeholder="描述"></textarea>
-              </div>
-              <div class="mb-3">
-                <label htmlFor="username">用户信息</label>
-                <div class="input-group">
-                  <input type="text" class="form-control" id="username" placeholder="用户信息...." />
-                </div>
-              </div>
-              <hr class="mb-4" />
-              <button class="btn btn-primary btn-lg btn-block" type="submit">提交</button>
-            </form>
-          </div> */}
-          {/* 更新数据 Ends */} -->
-        <!-- {/* 数据详情 Starts */} -->
         <div class="col-md-8 order-md-1">
-          <h4 class="mb-3">更新的数据内容</h4>
-          <div class="card">
-            <div class="card-header">数据名称</div>
-            <div class="card-body">
-              <blockquote class="blockquote mb-0">
-                <p>描述...</p>
-                <footer class="text-muted mb-2">类型</footer>
-              </blockquote>
-              <a href="#" class="btn btn-primary">编辑</a>
-            </div>
+            <h4 class="mb-3">更新数据</h4>
+            <ResourceUpdate />
           </div>
-        </div>
-        <!-- {/* 数据详情 Ends */} -->
+        <!-- 更新数据 Ends  -->
+        <!-- 数据详情 -->
+        <ResourceDetail />
       </div>
     </div>
 </template>
@@ -60,6 +26,8 @@
 import Header from "@/components/Header.vue";
 import ResourceSearch from "@/components/ResourceSearch.vue";
 import ResourceList from "@/components/ResourceList.vue";
+import ResourceUpdate from "@/components/ResourceUpdate.vue";
+import ResourceDetail from "@/components/ResourceDetail.vue";
 
 export default {
   name: "ResourceHome",
@@ -67,6 +35,8 @@ export default {
     Header,
     ResourceSearch,
     ResourceList,
+    ResourceUpdate,
+    ResourceDetail
   },
 }
 </script>
