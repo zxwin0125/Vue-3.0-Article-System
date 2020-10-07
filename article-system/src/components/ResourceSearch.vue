fcf7bd0d0546f0f207299b4644c99a91152b99aa
zxwin <zxwin_0125@163.com>
2020-10-07 22:37:27 +0800

    :fire:搜索功能

--- article-system/src/components/ResourceSearch.vue
+++ article-system/src/components/ResourceSearch.vue
@@ -2,10 +2,12 @@
   <!-- {/* 搜索框 start */} -->
   <form class="card p-2">
     <div class="input-group">
-      <input type="text" class="form-control" placeholder="写点啥..." />
-      <div class="input-group-append">
-        <button type="submit" class="btn btn-secondary">搜索</button>
-      </div>
+      <input
+        @keyup="$emit('onsearch', $event.target.value)"
+        type="text"
+        class="form-control"
+        placeholder="写点啥..."
+      />
     </div>
   </form>
   <!-- {/* 搜索框 Ends */} -->
