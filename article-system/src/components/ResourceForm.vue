7c1826bce76d75f5bad8b092fac03d40907a3b2e
zxwin <zxwin_0125@163.com>
2020-10-07 21:30:10 +0800

    :fire:重构更新组件

---
+++ article-system/src/components/ResourceForm.vue
@@ -0,0 +1,92 @@
+<template>
+  <form>
+    <div v-if="alert?.success" class="alert alert-success">
+      {{ alert.success }}
+    </div>
+    <div v-if="alert?.error" class="alert alert-danger">{{ alert.error }}</div>
+    <div class="mb-3">
+      <label for="title">标题</label>
+      <input
+        v-model="uResource.title"
+        type="text"
+        class="form-control"
+        id="title"
+        placeholder="title...."
+      />
+    </div>
+
+    <div class="mb-3">
+      <label for="description">描述</label>
+      <textarea
+        v-model="uResource.description"
+        class="form-control"
+        id="description"
+        placeholder="描述"
+      ></textarea>
+    </div>
+    <div class="mb-3">
+      <label for="type">类型</label>
+      <select class="form-control" id="type" v-model="uResource.type">
+        <option
+          v-for="(resourceType, index) in types"
+          :key="index"
+          :value="resourceType"
+        >
+          {{ resourceType }}
+        </option>
+      </select>
+    </div>
+    <div class="mb-3">
+      <label for="link">链接</label>
+      <div class="input-group">
+        <input
+          v-model="uResource.link"
+          type="text"
+          class="form-control"
+          id="link"
+          placeholder="链接...."
+        />
+      </div>
+    </div>
+    <hr class="mb-4" />
+    <button
+      @click="submitForm()"
+      class="btn btn-primary btn-lg btn-block"
+      type="button"
+    >
+      提交
+    </button>
+  </form>
+</template>
+
+<script>
+import { ref, watch } from "vue";
+
+export default {
+  props: {
+    resource: Object,
+    alert: Object,
+  },
+  setup(props, context) {
+    const uResource = ref(props.resource);
+    const types = ["blog", "video", "book"];
+
+    watch(
+      () => props.resource,
+      (resource, prevResource) => {
+        uResource.value = resource;
+      }
+    );
+
+    const submitForm = () => {
+      context.emit("onFormSubmit", uResource.value);
+    };
+
+    return {
+      uResource,
+      types,
+      submitForm,
+    };
+  },
+};
+</script>
--- article-system/src/components/ResourceUpdate.vue
+++ article-system/src/components/ResourceUpdate.vue
@@ -1,54 +1,9 @@
 <template>
-  <form>
-    <div v-if="alert?.success" class="alert alert-success">{{ alert.success }}</div>
-    <div v-if="alert?.error" class="alert alert-danger">{{ alert.error }}</div>
-    <div class="mb-3">
-      <label htmlFor="title">标题</label>
-      <input
-        v-model="uResource.title"
-        type="text"
-        class="form-control"
-        id="title"
-        placeholder="title...."
-      />
-    </div>
-    <div class="mb-3">
-      <label for="description">描述</label>
-      <textarea
-        v-model="uResource.description"
-        class="form-control"
-        id="description"
-        placeholder="描述"
-      ></textarea>
-    </div>
-    <div class="mb-3">
-      <label htmlFor="type">类型</label>
-      <select class="form-control" id="type" v-model="uResource.type">
-        <option v-for="(resourceType, index) in types" 
-                :key="index" 
-                :value="resourceType">{{resourceType}}</option>
-        
-      </select>
-
-    </div>
-    <div class="mb-3">
-      <label htmlFor="link">链接</label>
-      <div class="input-group">
-        <input
-          v-model="uResource.link"
-          type="text"
-          class="form-control"
-          id="link"
-          placeholder="链接...."
-        />
-      </div>
-    </div>
-    <hr class="mb-4" />
-    <button 
-      @click="handleUpdate" 
-      class="btn btn-primary btn-lg btn-block" 
-      type="button">提交</button>
-  </form>
+  <ResourceForm
+    @onFormSubmit="handleUpdate($event)"
+    :resource="resource"
+    :alert="alert"
+  />
 </template>
 
 <script>
@@ -81,8 +36,6 @@ export default {
           clearAlertTimeout()
           data.alert = initAlert()
         }
-
-        uResource.value = resource
       }
     )
 
@@ -136,7 +89,7 @@ export default {
       
     }
 
-    return { ...toRefs(data), uResource, types, handleUpdate }
+    return { ...toRefs(data), handleUpdate }
   }
 }
 </script>
