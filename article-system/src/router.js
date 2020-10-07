70896951f012e6ff614d0f2b28b755d90b641177
zxwin <zxwin_0125@163.com>
2020-10-07 22:04:30 +0800

    :fire:详情跳转

--- article-system/src/router.js
+++ article-system/src/router.js
@@ -1,63 +1,55 @@
 import { createRouter, createWebHistory } from "vue-router";
 import ResourceHome from "@/views/ResourceHome.vue";
 import ResourceNew from "@/views/ResourceNew.vue";
-import ResourceRoutes from "@/views/ResourceRoutes.vue"
+// import ResourceRoutes from "@/views/ResourceRoutes.vue";
+import ResourceDetailPage from "@/views/ResourceDetailPage.vue";
 
 const routes = [
-  // =================================================================
-  // { path: "/", component: ResourceHome },
-  // { path: "/new", component: ResourceNew}
-
-  // =================================================================
-  // 重定向、定义名字
   // http://localhost:8080/resources
-  { 
-    path: "/", 
-    name: "base", 
-    redirect: { name: "resourceHomePage"} 
+  // { path: "/", name: "base", redirect: "/resources" },
+  { path: "/", name: "base", redirect: { name: "resourceHomePage" } },
+  {
+    path: "/resources",
+    name: "resourceHomePage",
+    component: ResourceHome,
   },
-  { 
-    path: "/resources", 
-    name: "resourceHomePage", 
-    component: ResourceHome 
+  { path: "/resources/new", name: "resourceNewPage", component: ResourceNew },
+  // http://localhost:8080/resources/2
+  {
+    path: "/resources/:id",
+    name: "resourceDetailPage",
+    component: ResourceDetailPage,
   },
-  { 
-    path: "/resources/new", 
-    name: "resourceNewPage", 
-    component: ResourceNew
-  }
-
-  // =================================================================
-  // 子路由
-  // { 
-  //   path: "/", 
-  //   name: "base", 
-  //   redirect: { name: "resourceHomePage"} 
-  // },
-  // { 
-  //   path: "/resources", 
-  //   name: "resourceHomePage", 
-  //   component: ResourceRoutes,
-  //   children: [
-  //     { 
-  //       path: "",
-  //       name: "resourceHome",
-  //       component: ResourceHome,
-  //     },
-  //     { 
-  //       path: "new",
-  //       name: "resourceNewPage",
-  //       component: ResourceNew,
-  //     }
-    // ]
-  // }
 ];
+
+// const routes = [
+//   // http://localhost:8080/resources
+//   // { path: "/", name: "base", redirect: "/resources" },
+//   { path: "/", name: "base", redirect: { name: "resourceHomePage" } },
+//   {
+//     path: "/resources",
+//     name: "resourceHomePage",
+//     component: ResourceRoutes,
+//     children: [
+//       {
+//         path: "",
+//         name: "resourceHome",
+//         component: ResourceHome,
+//       },
+//       {
+//         path: "new",
+//         name: "resourceNewPage",
+//         component: ResourceNew,
+//       },
+//     ],
+//   },
+// ];
 
 const router = createRouter({
   history: createWebHistory(),
   //   routes: routes,
   routes,
-  linkExactActiveClass: "active",
+  linkActiveClass: "active",
 });
 
 export default router;
